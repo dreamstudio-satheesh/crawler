de6591ca2f6dbca8d1306e349d259d4e0f3b2c81
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-09-16 00:20:13 +0530

    app

--- app/Http/Controllers/ScrapedlinkController.php
+++ app/Http/Controllers/ScrapedlinkController.php
@@ -48,10 +48,10 @@ class ScrapedlinkController extends Controller
             if ($request->title) {
                 $node = $crawler->filter($request->title);
                 if ($node->count() > 0) {
-                    $data['title'] = $node->text();
-               } else {
-                    $data['title'] = "title not found";
-               }
+                        $data['title'] = $node->text();
+                } else {
+                        $data['title'] = "title not found";
+                }
                
             }
             if ($request->description) {
