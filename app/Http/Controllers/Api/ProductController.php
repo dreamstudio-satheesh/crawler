9b58d3416fe208903e9bf12702c2509c618b72bf
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-09-21 09:57:45 +0530

    app

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Productresource;
 use App\Http\Controllers\Api\BaseController;
 
 class ProductController extends BaseController
@@ -17,9 +18,27 @@ class ProductController extends BaseController
     public function search(Request $request)
     {
         $searchQuery = $request->input('query');
+        if ($searchQuery) {
+            $totalRecords = Product::where('name', 'LIKE', "%{$searchQuery}%")->count();
+            $skip = isset($request->page) ? $request->page : 0;
 
-        $products = Product::where('name', 'LIKE', "%{$searchQuery}%")->get();
+            $products = Product::where('name', 'LIKE', "%{$searchQuery}%")
+            ->offset($skip)
+            ->limit(10)
+            ->get();
 
-        return response()->json($products);
+            $balalceRecords = $totalRecords - ($skip + 10);
+            $nextpage ='false';
+            if ($balalceRecords > 0) {
+                $nextpage = 'true';
+            }
+
+            $message = $products ? 'data fetched' : 'No record found';  
+            return $this->sendResponse(Productresource::collection($products), $message,$nextpage);
+
+        //return response()->json($products);
+        }
+
+        
     }
 }
