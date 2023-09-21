fb117f88b8e75502884ae4d9b3aaff65259505d9
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-09-21 09:37:34 +0530

    app

--- app/Http/Middleware/IPCheckMiddleware.php
+++ app/Http/Middleware/IPCheckMiddleware.php
@@ -17,7 +17,7 @@ class IPCheckMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         $ip = $request->ip();
-        dd($ip);
+       // dd($ip);
         $apiKey = $request->header('X-API-KEY');
 
         $access = DB::table('allowed_access')
