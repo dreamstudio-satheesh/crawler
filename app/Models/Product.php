dbc81bda2284455708f8ad351b4822994bb2b48a
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-09-24 20:34:52 +0530

    app

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -13,6 +13,6 @@ class Product extends Model
 
     public function link()
     {
-        return $this->hasOne(ScrapedLink::class);
+        return $this->hasOne(ScrapedLink::class, 'id');
     }
 }
