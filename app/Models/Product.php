5a6a1d75f31e4626d1875d8b926c12e77958388d
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-10-05 17:51:01 +0530

    app

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -18,6 +18,6 @@ class Product extends Model
 
     public function website()
     {
-        return $this->belongsTo(Website::class,'website_id')->select(['id','url']);
+        return $this->belongsTo(Website::class,'website_id')->select(['id','name','url']);
     }
 }
